955f1bd127b9d944f67752c9fccd8ec03e86cf6a
EvertonM94 <evertonlom94@gmail.com>
2026-04-09 21:09:06 -0300

    Add files via upload

---
+++ introduçao javascript/aula007/script.js
@@ -0,0 +1,4 @@
+let numero = Number(prompt("Digite o valor em Real"))
+let dolar = numero /5.30
+
+alert(`Valor em Dolar ${dolar.toFixed(2)}`)
---
+++ introduçao javascript/aula008/script.js
@@ -0,0 +1,20 @@
+let idade = Number(prompt("Qual a sua Idade: "))
+let habilitado = prompt(`Você tem habilitação:
+    1 - sim
+    2 - não
+    `)
+
+    if(habilitado == "1") {
+        habilitado = true
+    } else{
+        habilitado= false
+    }
+    
+
+if (idade >= 18 && habilitado == true) {
+    alert("Pode dirigir !")
+    
+} else {
+    alert("Não pode dirigir!")
+    
+}
